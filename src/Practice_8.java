0d278d10cd0024ff1fb80a41770f9129ef3c7fda
Alekzander <alekzanderor@gmail.com>
2020-03-28 17:53:03 +0400

    Initial commit

---
+++ src/Practice_8.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Practice_8 {
+
+    public static void main(String[] args)
+    {
+          int i=1;
+        while (i<=50){
+           System.out.println(i);
+           i++;
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
